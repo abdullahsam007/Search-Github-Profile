34d0c960db1fbc58d4f5d899a5f00acde86c1256
AbdullahS <127679907+abdullahsam007@users.noreply.github.com>
2023-04-12 22:54:28 +0500

    Update script.js

--- Search Github/script.js
+++ Search Github/script.js
@@ -4,6 +4,33 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
+/*document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+
+})
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+        return false;
+    }
+
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+        return false;
+    }
+
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        return false;
+    }
+
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+        return false;
+    }
+}*/
+
+
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
